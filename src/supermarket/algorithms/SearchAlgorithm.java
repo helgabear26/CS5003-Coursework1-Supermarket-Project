83b1999c8533c7d28dc71da86a3ed57c3cf3c50f
emmanuelmeg <chimereleo151@gmail.com>
2025-11-20 09:04:08 +0000

    this is it

--- src/supermarket/algorithms/SearchAlgorithm.java
+++ src/supermarket/algorithms/SearchAlgorithm.java
@@ -1,4 +1,5 @@
 package supermarket.algorithms;
 
 public class SearchAlgorithm {
+
 }
--- src/supermarket/inventory/Activity.java
+++ src/supermarket/inventory/Activity.java
@@ -1,4 +1,83 @@
 package supermarket.inventory;
 
+import java.time.LocalDateTime;
+
+
 public class Activity {
+    private int quantity;
+    private String name;
+    private LocalDateTime TimeStamp;
+    private String id;
+
+    public Activity(int quantity, String name, LocalDateTime TimeStamp, String id) {
+        this.quantity = quantity;
+        this.TimeStamp = TimeStamp;
+        this.name = name;
+        this.id = id;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return TimeStamp;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        TimeStamp = timeStamp;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void AddToStock(int value) {
+        if (value > 0) {
+            quantity += value;
+            name = "addToStock";
+            TimeStamp = LocalDateTime.now();
+        }
+    }
+
+    public void RemoveFromStock(int value) {
+        if (value > 0 && quantity >= 0) {
+            quantity -= value;
+            name = "Remove from stock ";
+            TimeStamp = LocalDateTime.now();
+
+        } else {
+            System.out.println(" there is not enough stock to remove or the stock is empty");
+        }
+
+
+    }
+
+    public String toString() {
+        return "Activity{" +
+                "ID = " + id + '\'' +
+                ", name= " + name +
+                ",Quantity = " + quantity +
+                ", data = " + TimeStamp +
+                '}';
+    }
 }
