2d3728922d81993c109c96eb7a33dddd7b43956f
helgabear26 <helgabearvvpc@gmail.com>
2025-11-20 19:01:24 +0000

    Commit after doing the Product class + CustomLinkedList class + comments to both

--- src/supermarket/inventory/Product.java
+++ src/supermarket/inventory/Product.java
@@ -4,41 +4,85 @@ import supermarket.management.CustomLinkedList;
 
 import java.time.LocalDateTime;
 
+// This class represents a single product in the supermarket system
+// Each product has:
+// - a product ID
+// - a name
+// - The date it was added to the system
+// - A quantity (current stock)
+// Alist of the last 4 activities using the custom linked list
+
 public class Product {
 
+    // A unique string that identifies this product
     private int quantity;
+
+    // The product's name
     private String name;
+
+    // Date and time when this product was created and added to the system
     private LocalDateTime TimeStamp;
+
+    // The number of items currently in stock
     private String id;
 
-    private CustomLinkedList<Activity> activities = new CustomLinkedList<>();
+    // A linked list that stores the last four activities objects performed for this product.
+    private CustomLinkedList activities = new CustomLinkedList();
+
+    // Constructor: Creates a new Product object
 
     public Product( int quantity, String name, String id){
+
+        // Sets the ID from the value passed to the constructor
         this.id = id;
+
+        // Sets the product name
         this.name = name;
+
+        // Sets the initial quantity
         this.quantity = quantity;
-        this.TimeStamp = LocalDateTime.now();
+
+        // Sets the entry date to the right now
+        this.TimeStamp = LocalDateTime.now(); // Captures the exact date and time the object was created
     }
 
-     // Keeps the last 4 activities
+     // Method: addActivity(Activity activity) adds a new activity to the list and ensures only four remain
     public void addActivity(Activity activity) {
+
+        // Add the new activity to the end of the linked list
         activities.add(activity);
+
+        // If more than four activities are stored
         if (activities.size() > 4) {
-            activities.removeFirst();
+
+            // Removes the first oldest activity
+            activities.removeFirstElement();
         }
     }
 
+    // Method: updateQuantity(int amount, String activityType) updates the quantity based on AddToStock and RemoveFromStock
+
     public void update_Quantity(int amount, String activityType){
+
+        // Checks if items are being added to stock
         if(activityType.equalsIgnoreCase("AddToStock")){
-            quantity += amount;
+            quantity += amount; // increases stock
+
+            // Check if items are being removed from stock
         } else if (activityType.equalsIgnoreCase("RemoveFromStock")) {
+
+            // Prevents negative stock number
             if (quantity - amount < 0) {
                 System.out.println("ERROR: Not enough stock. ");
                 return;
             }
+
+            // Removes items from stock
             quantity -= amount;
         }
     }
+
+    // Getters return the linked list of activities
 
     public String getId(){
         return id;
@@ -57,9 +101,13 @@ public class Product {
     }
 
 
-    public CustomLinkedList<Activity> getActivities(){
+    public CustomLinkedList getActivities(){
+
         return activities;
     }
+
+
+    // Method: toString() returns a text version of the product to print product details
 
 
     @Override
--- src/supermarket/management/CustomLinkedList.java
+++ src/supermarket/management/CustomLinkedList.java
@@ -1,4 +1,145 @@
 package supermarket.management;
 
+// This class is an implementation of a custom Linked List
+// LinkedLists store nodes in two parts being:
+// 1. Data about the object you want to store
+// 2. An address which is a link between the nodes in the chain
+
 public class CustomLinkedList {
+
+    // Below is the inner class Node
+    // A node represents an element inside a linked list. It contains:
+    // 1. data
+    // 2. a reference to the next node in the list
+
+    private class Node {
+
+        // This variable stores the data that needs to be kept in the list.
+        // It is of type object so it can store any type(e.g Activity, Product, String)
+
+        Object data;
+
+        // This variable stores a reference to the next node in the list
+        // If next = null, it means this is the last node
+
+        Node next;
+
+        // Constructor - this method creates a new Node object
+        // It takes the argument data which is the thing we want to store
+
+        Node(Object data) {
+
+            // Store the passed data inside the node
+
+            this.data = data;
+
+            // Set next to null because when the node is first created it doesn't point to anything yet.
+
+            this.next = null;
+
+        }
+    }
+
+    // Linked List state variables
+
+    // Assigns the first node in the linked list
+    // If head == null, it means the list is empty
+
+    private Node head;
+
+    // the last node in the list
+
+    private Node tail;
+
+    // States how may nodes are currently available in the list
+
+    private int size;
+
+    // Method: add(ObjectData) adds a new element to the end of the linked list
+
+    public void add(Object data) {
+
+        // Creates anew node containing the new data
+
+        Node newNode = new Node(data);
+
+        // Case 1: If the list is empty (head == null), then the new node becomes BOTH the first and last node
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+
+        // Case 2: List is not empty so a new node is attached to the end of the list as new data is added
+
+        } else {
+
+            // Makes the current last node point to the new node
+
+            tail.next = newNode;
+
+            // Updates the tail reference so it points to the new node
+            tail = newNode;
+        }
+
+        // Increase the size of the counter because a new node was added
+        size++;
+
+    }
+
+    // Method: removeFirst() removes the first element from the linked list which allows only the last four activities to be kept
+
+    public void removeFirstElement() {
+
+        // If head is null, the lsit is empty, so nothing to remove
+
+        if (head == null)
+            return;
+
+        // Moves the head to the next node
+        // The old first node gets removed
+
+        head = head.next;
+
+        // Decrease the size counter
+
+        size--;
+
+        // If the list becomes empty after removal the tail must also be set to null
+
+        if (head == null) {
+            tail = null;
+        }
+    }
+
+    // Method get(int index) returns the data object stored at a specific position
+
+    public Object get(int index) {
+
+        // If index is invalid (as in less than 0 or greater than the last index), it throws an error to prevent the accessing of memory illegally
+
+        if (index < 0 || index >= size) // uses a logical OR operator
+            throw new IndexOutOfBoundsException();
+
+        // Start at the head of the list
+
+        Node current = head;
+
+        // Move step-by-step until we reach the desired index
+
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        // Return the data stored in the node
+
+        return current.data;
+    }
+
+    // Method: getSize() returns the amount of elements there are in the lsit
+
+    public int size(){
+        return size;
+    }
+
+
 }
